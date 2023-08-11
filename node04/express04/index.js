08b10da77f17f71b88cd11799c8b9646e3cf7674
jjjj88521 <jjjj88521@gmail.com>
2023-08-11 20:58:55 +0800

    20230811 node04hw

--- node04/express04/index.js
+++ node04/express04/index.js
@@ -11,14 +11,38 @@ app.get("/p/:id/", (req, res) => {
     let pageName = "";
     if (id === "CvRz0e3Awmi") {
         pageName =
-            "《ぷるんぷるんすぎるマシュマロアイス(Too plump marshmallow ice cream in Japan)》";
+            "ぷるんぷるんすぎるマシュマロアイス(Too plump marshmallow ice cream in Japan)<img src='https://images.pexels.com/photos/7539498/pexels-photo-7539498.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'>";
     } else if (id === "CvZP-PIguWG") {
-        pageName = `浅草で一番おすすめしたい抹茶クレープ(The most recommended matcha crepe in Asakusa)
-            <img src="https://images.pexels.com/photos/1716861/pexels-photo-1716861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">`;
+        pageName =
+            "浅草で一番おすすめしたい抹茶クレープ(The most recommended matcha crepe in Asakusa)";
     }
     res.end(pageName);
 });
 
+app.get("/books/:cate/:bookid", (req, res) => {
+    res.setHeader("content-type", "text/html;charset=utf-8");
+    let cate = req.params.cate;
+    let bookid = req.params.bookid;
+    res.end(`分類id ${cate}; 書本編號 ${bookid}`);
+});
+
+app.get("/user/:name?", (req, res) => {
+    res.setHeader("content-type", "text/html;charset=utf-8");
+    let name = req.params.name;
+    if (name) {
+        res.end(`你好, ${name}`);
+    } else {
+        res.end(`你好, 不認識的人`);
+    }
+});
+
+// * 代表後面所有的路徑不管幾層不管名字都可以
+app.get("/file/*", (req, res) => {
+    res.setHeader("content-type", "text/html;charset=utf-8");
+    let name = req.params[0];
+    res.end(`檔案是 ${name}`);
+});
+
 app.listen(3000, () => {
-    console.log("伺服器開啟");
+    console.log("server is running");
 });
--- node04/node04hw01/index.js
+++ node04/node04hw01/index.js
@@ -8,17 +8,26 @@ app.get("/", (req, res) => {
     res.end(data);
 });
 
-app.get("/photos/:id/pexels-photo-:id.jpeg", (req, res) => {
+app.get("/photos/:id/pexels-photo-:id.jpeg", (req, res, next) => {
     const id = req.params.id;
     const ids = ["1716861", "6397725", "7465580", "7465582", "4188296"];
     let pageName = "";
     res.setHeader("content-type", "text/html;charset=utf-8");
     if (ids.includes(id)) {
-        pageName = `<img src="https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">`;
+        pageName = `<img src="https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&w=800&h=750&dpr=1">`;
+    } else {
+        // 將控制權交給下個中介軟體或路由處理
+        next();
     }
     res.end(pageName);
 });
 
+app.use("/*", (req, res) => {
+    res.type("text/html");
+    res.status(404);
+    res.end("<h1>404 - Not Found</h1>");
+});
+
 app.listen(3000, () => {
-    console.log("伺服器開啟");
+    console.log("原神啟動");
 });
